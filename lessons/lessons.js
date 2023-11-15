b1f4cec29ec8a1bf29e8184d076e8ffef62f1f3f
thefathima <fathima_riffai@outlook.com>
2023-11-16 02:10:36 +0400

    linking the stylesheet & js file

---
+++ lessons/lessons.js
@@ -0,0 +1,18 @@
+let lessons = [ //an array of lessons with various attributes (JSON objects)
+    {
+        "name": "History",
+        "location": "Dubai Marina",
+        "price": 20,
+        "availability": 5,
+        "image": "../images/history.png",
+        "rating": 4,
+    },
+    {
+        "name": "Geography",
+        "location": "Palm Jumeirah",
+        "price": 10,
+        "availability": 5,
+        "image": "../images/globe.gif",
+        "rating": 3,
+    }
+]
